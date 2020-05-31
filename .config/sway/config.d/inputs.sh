fda4f90810436438edd25ad03c2af8a6e93ed696
Mike Wallio <rawrizaur@gmail.com>
2020-05-31 14:29:00 -0400

    Update keyboard bindings

--- .config/sway/config.d/inputs.sh
+++ .config/sway/config.d/inputs.sh
@@ -13,18 +13,20 @@ input * {
 bindsym --locked XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume $(pacmd list-sinks |awk '/* index:/{print $3}') +5%
 bindsym --locked XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume $(pacmd list-sinks |awk '/* index:/{print $3}') -5%
 bindsym --locked XF86AudioMute exec --no-startup-id pactl set-sink-mute $(pacmd list-sinks |awk '/* index:/{print $3}') toggle
+bindsym --locked XF86AudioMicMute exec amixer set Capture toggle
+
 bindsym --locked XF86AudioPlay exec playerctl play-pause
 bindsym --locked XF86AudioNext exec playerctl next
 bindsym --locked XF86AudioPrev exec playerctl previous
-bindsym --locked XF86AudioMicMute exec amixer set Capture toggle
-
-# Multi-Function Keys
-bindsym --locked XF86WLAN exec [[ "$(nmcli radio wifi)" == "enabled" ]] && nmcli radio wifi off || nmcli radio wifi on
-bindsym --locked XF86Bluetooth exec blueman-assistant
 
 # Brightness controls
 bindsym --locked XF86MonBrightnessUp exec --no-startup-id light -A 5
 bindsym --locked XF86MonBrightnessDown exec --no-startup-id light -U 5
+
+# Multi-Function Keys
+bindsym --locked XF86WLAN exec [[ "$(nmcli radio wifi)" == "enabled" ]] && nmcli radio wifi off || nmcli radio wifi on
+bindsym --locked XF86Tools exec gnome-control-center
+bindsym --locked XF86Bluetooth exec gnome-control-center bluetooth
 
 bindsym $mod+Shift+Print exec grim -g "$(slurp)" $(xdg-user-dir PICTURES)/$(date +'%Y-%m-%d-%H%M%S_grim.png')
 
