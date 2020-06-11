8a0631a759003841fe08fd7258fc8c17253f3bba
Mike Wallio <rawrizaur@gmail.com>
2020-06-10 21:24:37 -0400

    Adjust gnome settings

--- .config/sway/config.d/looks.sh
+++ .config/sway/config.d/looks.sh
@@ -12,9 +12,3 @@ for_window [class="(Code)"] opacity $opacity
     smart_gaps on
     gaps inner 4
     gaps outer 2
-
-# Import GNOME settings
-exec_always import-gsettings \
-    gtk-theme:gtk-theme-name \
-    icon-theme:gtk-icon-theme-name \
-    cursor-theme:gtk-cursor-theme-name
