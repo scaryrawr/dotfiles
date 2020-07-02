1c80a31877b9e7db08ba405efdefd3af9c84adc4
Mike Wallio <rawrizaur@gmail.com>
2020-07-02 15:46:27 -0400

    Update to ignore teams notifications

--- .config/sway/config.d/looks.sh
+++ .config/sway/config.d/looks.sh
@@ -7,3 +7,6 @@
     gaps inner 8
 
 exec --no-startup-id ${HOME}/.config/sway/scripts/sway_inactive &
+
+no_focus [title="Microsoft Teams Notification"]
+for_window [title="Microsoft Teams Notification"] move position 20 20
