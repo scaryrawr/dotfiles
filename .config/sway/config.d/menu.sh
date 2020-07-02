5a605588a9f6df176081dfdf08fc33515477472a
Mike Wallio <rawrizaur@gmail.com>
2020-07-02 11:43:43 -0400

    Fix launching of some apps

--- .config/sway/config.d/menu.sh
+++ .config/sway/config.d/menu.sh
@@ -3,4 +3,4 @@
 # on the original workspace that the command was run on.
 
 for_window [app_id="tofi"] floating enable, border pixel 2, sticky enable
-set $menu exec alacritty --class tofi -d 80 10 -e sh -c '$HOME/.config/sway/scripts/tofi | sort -u -r | fzf | $HOME/.config/sway/scripts/tofi'
+set $menu exec alacritty --class tofi -d 80 10 -e sh -c '$HOME/.config/sway/scripts/tofi | sort -u -r | fzf | $HOME/.config/sway/scripts/tofi | xargs swaymsg exec --'
