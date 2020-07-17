876fcfbfa4e9377bbec02b91e93f70aab0a672ed
Mike Wallio <michael.wallio@outlook.com>
2020-07-17 15:40:42 -0400

    Test not working...

--- .config/fish/conf.d/gnome-keyring.fish
+++ .config/fish/conf.d/gnome-keyring.fish
@@ -1,1 +1,1 @@
-test gnome-keyring-daemon && set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start | awk -F '=' '/SSH_AUTH_SOCK/ {print $2}')
+set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start | awk -F '=' '/SSH_AUTH_SOCK/ {print $2}')
