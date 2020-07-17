0f240439566e63d7f5ca1e75e5987c3cf6490397
Mike Wallio <michael.wallio@outlook.com>
2020-07-17 15:15:12 -0400

    Tweak teams settings

--- .config/sway/config.d/looks.sh
+++ .config/sway/config.d/looks.sh
@@ -9,4 +9,4 @@
 exec --no-startup-id ${HOME}/.config/sway/scripts/sway_inactive &
 
 no_focus [title="Microsoft Teams Notification"]
-for_window [title="Microsoft Teams Notification"] move position 20 20
+for_window [title="Microsoft Teams Notification"] floating enable, move position 20 20
