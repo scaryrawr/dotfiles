72a4e58805dc3990075becc6af7ab3d1c73c9d21
Mike Wallio <michael.wallio@outlook.com>
2020-07-21 17:37:20 -0400

    Add comment explaining fish wal

--- .config/wal/templates/colors.fish
+++ .config/wal/templates/colors.fish
@@ -1,7 +1,12 @@
+# Colors numbers based off agnoster default Colors
+#
+# Using `cat ~/.cache/wal/sequences` in fish ends up causing
+# issues for other tools like fzf (noticeable when using reload)
+
 set color_virtual_env_bg "{color7}"
 set color_virtual_env_str "{color0}"
 set color_user_bg "{color0}"
-set color_user_str "{color11}"
+set color_user_str "{color3}"
 set color_dir_bg "{color4}"
 set color_dir_str "{color0}"
 set color_hg_changed_bg "{color3}"
@@ -19,4 +24,4 @@ set color_status_nonzero_str "{color1}"
 set color_status_superuser_bg "{color0}"
 set color_status_superuser_str "{color3}"
 set color_status_jobs_bg "{color0}"
-set color_status_jobs_str "{color12}"
+set color_status_jobs_str "{color14}"
