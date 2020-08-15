acf846e27b390decfb28fc57f1daa17ea49a3418
Mike Wallio <michael.wallio@outlook.com>
2020-08-15 17:13:00 -0400

    Use alacritty

--- .config/sway/config.d/menu.sh
+++ .config/sway/config.d/menu.sh
@@ -2,5 +2,5 @@
 # Note: pass the final command to swaymsg so that the resulting window can be opened
 # on the original workspace that the command was run on.
 
-for_window [instance="tofi"] floating enable, border pixel 2, sticky enable
-set $menu exec urxvt  -title tofi -name tofi -geometry 80x10 -e sh -c '${HOME}/GitHub/tofi/tofi'
+for_window [app_id="tofi"] floating enable, border pixel 2, sticky enable
+set $menu exec alacritty  --title tofi --class tofi -d 80 10 -e sh -c '${HOME}/GitHub/tofi/tofi', sticky enable
