8cb37c0ee285db81201639bc4de6dc0b20bc04c9
Mike Wallio <michael.wallio@outlook.com>
2020-09-30 14:41:40 -0400

    Remove &'s

--- .config/sway/config.d/menu.sh
+++ .config/sway/config.d/menu.sh
@@ -6,4 +6,4 @@
 #set $menu exec alacritty  --title tofi --class tofi -d 80 10 -e sh -c '${HOME}/GitHub/tofi/tofi', sticky enable
 for_window [instance="tofi"] floating enable, border pixel 2, sticky enable
 set $menu exec urxvt  -title tofi -name tofi -geometry 80x10 -e sh -c '${HOME}/GitHub/tofi/tofi', sticky enable
-exec ~/.local/bin/smartfloat tofi &
+exec ~/.local/bin/smartfloat tofi
