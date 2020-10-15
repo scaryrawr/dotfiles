cf6b82a35c0e6e9ff4bd8e4315553311ff19ef32
Mike Wallio <michael.wallio@outlook.com>
2020-10-14 22:08:29 -0400

    Switch to redshift with wayland support

--- .config/sway/config.d/outputs.sh
+++ .config/sway/config.d/outputs.sh
@@ -7,5 +7,4 @@ output eDP-1 resolution 1920x1080 position 0,0
 output DP-2 resolution 3840x1600 position 1920,0
 
 exec /usr/libexec/geoclue-2.0/demos/agent
-
-exec gammastep -t 5700:3500 -m wayland -l geoclue2
+exec redshift-gtk -t 5700:3500 -m wayland -l geoclue2
