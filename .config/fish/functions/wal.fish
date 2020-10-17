65cf8f56ae1697579b4bae95cc19bc0e1639638f
Mike Wallio <michael.wallio@outlook.com>
2020-10-16 20:09:22 -0400

    Add bat wal theme

--- .config/fish/functions/wal.fish
+++ .config/fish/functions/wal.fish
@@ -2,15 +2,23 @@ function wal --description "wal wrapper"
 	command wal $argv
 
 	if ! test -e ~/.config/mako/config
+		mkdir -p ~/.config/mako
 		ln -s ~/.cache/wal/mako.conf ~/.config/mako/config
 	end
 
 	if ! test -e ~/.config/dunst/dunstrc
+		mkdir -p ~/.config/dunst
 		ln -s ~/.cache/wal/dunstrc ~/.config/dunst/dunstrc
 	end
 
 	if ! test -e ~/.config/fish/conf.d/colors.fish
+		mkdir -p ~/.config/fish.conf.d
 		ln -s ~/.cache/wal/colors.fish ~/.config/fish/conf.d/colors.fish
+	end
+
+	if ! test -e ~/.config/bat/themes/wal.tmTheme
+		mkdir -p ~/.config/bat/themes
+		ln -s ~/.cache/wal/wal.tmTheme ~/.config/bat/themes/wal.tmTheme
 	end
 
 	if pgrep mako > /dev/null
--- .config/zsh/functions/wal.zsh
+++ .config/zsh/functions/wal.zsh
@@ -1,17 +1,27 @@
+#!/usr/bin/env zsh
+
 # Helper for running wal and setting up links
 function wal() {
 	command wal $@
 
 	if [ ! -f ~/.config/mako/config ]; then
+		mkdir ~/.config/mako
 		ln -s ~/.cache/wal/mako.conf ~/.config/mako/config
 	fi
 
 	if [ ! -f ~/.config/dunst/dunstrc ]; then
+		mkdir ~/.config/dunst
 		ln -s ~/.cache/wal/dunstrc ~/.config/dunst/dunstrc
 	fi
 
 	if [ ! -f ~/.config/fish/conf.d/colors.fish ]; then
+		mkdir -p ~/.config/fish/conf.d
 		ln -s ~/.cache/wal/colors.fish ~/.config/fish/conf.d/colors.fish
+	fi
+
+	if [ ! -f ~/.config/bat/themes/wal.tmTheme ]; then
+		mkdir -p ~/.config/bat/themes
+		ln -s ~/.cache/wal/wal.tmTheme ~/.config/bat/themes/wal.tmTheme
 	fi
 
 	if pkill dunst; then
