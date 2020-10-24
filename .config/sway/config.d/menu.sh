4c1905afa2b62e9ae0eb63c1b357a46829fa6d01
Mike Wallio <michael.wallio@outlook.com>
2020-10-24 11:38:03 -0400

    Use C++ tofi installed

--- .config/sway/config.d/menu.sh
+++ .config/sway/config.d/menu.sh
@@ -2,8 +2,12 @@
 # Note: pass the final command to swaymsg so that the resulting window can be opened
 # on the original workspace that the command was run on.
 
-#for_window [app_id="tofi"] floating enable, border pixel 2, sticky enable
-#set $menu exec alacritty  --title tofi --class tofi -d 80 10 -e sh -c '${HOME}/GitHub/tofi/tofi', sticky enable
-for_window [instance="tofi"] floating enable, border pixel 2, sticky enable
-set $menu exec urxvt  -title tofi -name tofi -geometry 80x10 -e sh -c '${HOME}/GitHub/tofi/tofi', sticky enable
+for_window [app_id="tofi"] floating enable, border pixel 2, sticky enable
+set $menu exec alacritty  --title tofi --class tofi -d 80 10 -e sh -c 'tofi', sticky enable
+
+# for_window [instance="tofi"] floating enable, border pixel 2, sticky enable
+# set $menu exec urxvt  -title tofi -name tofi -geometry 80x10 -e sh -c 'tofi', sticky enable
+
 exec ~/.local/bin/smartfloat tofi
+
+# set $menu wofi
