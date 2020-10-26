834ee52a294472c89f6cdf4ef4db49446eae6f60
Mike Wallio <michael.wallio@outlook.com>
2020-10-25 23:01:45 -0400

    Update for tofi options

--- .config/sway/config.d/menu.sh
+++ .config/sway/config.d/menu.sh
@@ -3,10 +3,10 @@
 # on the original workspace that the command was run on.
 
 for_window [app_id="tofi"] floating enable, border pixel 2, sticky enable
-set $menu exec alacritty  --title tofi --class tofi -d 80 10 -e sh -c 'tofi', sticky enable
+set $menu exec alacritty  --title tofi --class tofi -d 80 10 -e zsh -c 'tofi --modes drun,run,i3wm', sticky enable
 
 # for_window [instance="tofi"] floating enable, border pixel 2, sticky enable
-# set $menu exec urxvt  -title tofi -name tofi -geometry 80x10 -e sh -c 'tofi', sticky enable
+# set $menu exec urxvt  -title tofi -name tofi -geometry 80x10 -e zsh -c 'tofi --modes drun,run,i3wm', sticky enable
 
 exec ~/.local/bin/smartfloat tofi
 
