e72d8cd651ad8af54a092edb982adf42cb1b0193
Mike Wallio <michael.wallio@outlook.com>
2021-01-29 13:22:44 -0500

    Merge and sort floating and non floating windows

--- .config/rofi/scripts/i3windows.sh
+++ .config/rofi/scripts/i3windows.sh
@@ -3,11 +3,13 @@
 # This script is to add window switching support in rofi for sway/i3
 
 if [ -z "$@" ]; then
-	# Tiling Windows
-	i3-msg -t get_tree | jq -r 'recurse(.nodes[]) | select(.type == "con") | select(.name) | .name'
+	{
+		# Tiling Windows
+		i3-msg -t get_tree | jq -r 'recurse(.nodes[]) | select(.type == "con") | select(.name) | .name'
 
-	# Floating Windows
-	i3-msg -t get_tree | jq -r '..| select(.floating_nodes? != null) | select(.floating_nodes != []) | .floating_nodes | .[].name'
+		# Floating Windows
+		i3-msg -t get_tree | jq -r '..| select(.floating_nodes? != null) | select(.floating_nodes != []) | .floating_nodes | .[].name'
+	} | sort -n
 else
 	# Try to "sanitize" the string for sending to sway/i3, need to remove double quotes as well as some regex patterns...
 	TITLE="$(echo $@ | sed 's/[\]\[)(\"\$^\\+?|]/./g')"
