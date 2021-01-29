acdc06d3ed795951363e6c3bf019db983e483b4c
Mike Wallio <michael.wallio@outlook.com>
2021-01-29 12:09:05 -0500

    Use rofi for sway

--- .config/sway/config.d/menu.sh
+++ .config/sway/config.d/menu.sh
@@ -2,12 +2,13 @@
 # Note: pass the final command to swaymsg so that the resulting window can be opened
 # on the original workspace that the command was run on.
 
-for_window [instance="tofi"] floating enable, border pixel 2, sticky enable
-for_window [app_id="tofi"] floating enable, border pixel 2, sticky enable
+#for_window [instance="tofi"] floating enable, border pixel 2, sticky enable
+#for_window [app_id="tofi"] floating enable, border pixel 2, sticky enable
 
-set $menu exec alacritty  --title tofi --class tofi -o window.dimensions.columns=80 -o window.dimensions.lines=15 -e zsh -c 'tofi --modes drun,run,i3wm'
+#set $menu exec alacritty  --title tofi --class tofi -o window.dimensions.columns=80 -o window.dimensions.lines=15 -e zsh -c 'tofi --modes drun,run,i3wm'
 # set $menu exec urxvt  -title tofi -name tofi -geometry 80x10 -e zsh -c 'tofi --modes drun,run,i3wm'
 
-exec ${HOME}/.local/bin/smartfloat tofi
+#exec ${HOME}/.local/bin/smartfloat tofi
 
-# set $menu wofi
+set $menu rofi -monitor $(xrandr | rg 'connected ' | awk '{if(line==NR) print $1}' line=$(swaymsg -t get_outputs | jq '.[].focused' | awk '{if($0=="true") print NR}')) -show
+#set $menu wofi
