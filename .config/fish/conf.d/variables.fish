88a947f036ed21eea1c7087fd041b47ad5ac12c4
Mike Wallio <michael.wallio@outlook.com>
2021-02-04 17:53:22 -0500

    Export to child processes and add fcitx

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -1,14 +1,27 @@
 if test -d $HOME/.local/bin
-  set PATH $PATH $HOME/.local/bin
+  set -gx PATH $PATH $HOME/.local/bin
 end
 
 if test -d $HOME/.cargo/bin
-  set PATH $PATH $HOME/.cargo/bin
+  set -gx PATH $PATH $HOME/.cargo/bin
 end
 
 if test -d $HOME/.dotnet/tools
-  set PATH $PATH $HOME/.dotnet/tools
+  set -gx PATH $PATH $HOME/.dotnet/tools
+end
+
+if test -d $HOME/bin
+  set -gx PATH $PATH $HOME/bin
 end
 
 set NPM_DIR (which npm)
-set NPM_DIR (dirname $NPM_DIR)
+set -gx NPM_DIR (dirname $NPM_DIR)
+
+if test "$XDG_SESSION_DESKTOP" = "sway"
+  set -gx XDG_CURRENT_DESKTOP sway
+  set -x QT_IM_MODULE fcitx
+  set -x GTK_IM_MODULE fcitx
+  set -x XMODIFIERS '@im=fcitx'
+  set -x XIM_PROGRAM fcitx
+  set -x SDL_IM_MODULE fcitx
+end
