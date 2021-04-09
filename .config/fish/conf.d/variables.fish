b745adbeb6fbb9a8c62eeb846db034282ef4be45
Mike Wallio <michael.wallio@outlook.com>
2021-04-08 20:22:10 -0400

    Fix path

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -1,13 +1,17 @@
 if test -d $HOME/.local/bin
-  set PATH $PATH $HOME/.local/bin
+  set -gx PATH $PATH $HOME/.local/bin
 end
 
 if test -d $HOME/.cargo/bin
-  set PATH $PATH $HOME/.cargo/bin
+  set -gx PATH $PATH $HOME/.cargo/bin
 end
 
 if test -d $HOME/.dotnet/tools
-  set PATH $PATH $HOME/.dotnet/tools
+  set -gx PATH $PATH $HOME/.dotnet/tools
+end
+
+if test -d $HOME/bin
+  set -gx PATH $PATH $HOME/bin
 end
 
 set NPM_DIR (which npm)
